4511e1bc7e195765af97caa2083161891acf6fe8
turtlebasket <turtlebasket@gmail.com>
2022-04-08 02:52:07 -0700

    pwa, take 2

--- static/sw.js
+++ static/sw.js
@@ -1,25 +1,24 @@
 const cacheItems = [
-    'index.html',
-    'index.js',
-    'styles.css',
-    'icon/basket_large.png',
-    'icon/basket.png',
-    'lib/jquery.min.js'
-  ]
+    '*.js',
+    '*.html',
+    '*.png',
+    'fonts',
+    '_app,'
+]
   
-  self.addEventListener('install', function(event) {
+self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('sw-cache').then(function(cache) {
-        return cache.addAll(cacheItems)
-        // return cache.add('index.html');
-      })
+        caches.open('sw-cache').then(function(cache) {
+            return cache.addAll(cacheItems)
+            // return cache.add('index.html');
+        })
     );
-  });
-   
-  self.addEventListener('fetch', function(event) {
+});
+
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
     );
-  });
+});
